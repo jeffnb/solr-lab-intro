560bb91669a2f314a48f947bd0ce5098d7c0450e
jeffnb <jeffnb@gmail.com>
2017-04-12 22:02:47 -0700

    Basic layout

--- projects/listings/src/main/java/Main.java
+++ projects/listings/src/main/java/Main.java
@@ -1,11 +1,17 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.text.WordUtils;
+import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.common.SolrInputDocument;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.*;
 
 
 public class Main {
@@ -18,6 +24,60 @@ public class Main {
         while(input.hasNext()) {
             listings.add(mapper.readValue(input.nextLine(), Listing.class));
         }
+
+
+        //Stand alone server example
+        String urlString = "http://localhost:8983/solr/listings";
+        SolrClient solr = new HttpSolrClient.Builder(urlString).build();
+
+        // Make a date formatter
+        DateTimeFormatter formatter =
+                DateTimeFormatter.ofLocalizedDateTime( FormatStyle.MEDIUM ).withLocale( Locale.US ).withZone( ZoneId.systemDefault() );
+
+
+        //Create all the documents
+        ArrayList<SolrInputDocument> docs = new ArrayList<>();
+        Set<Long> ids = new HashSet<>();
+        for(Listing l : listings){
+            SolrInputDocument doc = l.createSolrDoc();
+
+            if(l.originalCreation != null){
+                Instant instant = Instant.ofEpochSecond(l.originalCreation);
+                doc.addField("createdFormatted", formatter.format(instant));
+            }
+
+            if(l.lastModified != null){
+                Instant instant = Instant.ofEpochSecond(l.lastModified);
+                doc.addField("modifiedFormatted", formatter.format(instant));
+            }
+
+            if(l.views != null && l.views > 0 && l.numFavorers != null && l.numFavorers > 0){
+                double popularity = (double)l.numFavorers / l.views;
+                doc.addField("popularity", popularity);
+            }
+
+
+            if(l.whenMade != null){
+                String made = "";
+                if(l.whenMade.equals("made_to_order")){
+                    made = "Made to Order";
+                }
+                else{
+                    made = WordUtils.capitalize(l.whenMade.replace("_", " - "));
+                }
+
+                doc.addField("madeFacet", made);
+            }
+
+            try {
+                solr.add(doc);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            ids.add(l.listingId);
+        }
+        solr.commit();
+        System.out.println("Total ids:" + ids.size());
 
     }
 }
