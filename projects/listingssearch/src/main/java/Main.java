798a4f26ac6b84014a47d7559573de6c7231f7bb
jeffnb <jeffnb@gmail.com>
2017-04-13 06:18:17 -0700

    Adding listing search base

---
+++ projects/listingssearch/src/main/java/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by omni on 4/13/17.
+ */
+public class Main {
+
+    public static final String serverUrl = "http://solr.omnilabsinc.com:8983/solr/listings";
+    public static final String localUrl = "http://localhost:8983/solr/listings";
+
+    public static void main(String[] args) {
+
+    }
+
+}
