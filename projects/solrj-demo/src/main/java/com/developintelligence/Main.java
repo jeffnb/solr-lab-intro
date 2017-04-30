1a1570c4dda8962ad42a6d9f33676ad3c265feac
jeffnb <jeffnb@gmail.com>
2017-04-30 12:09:26 -0700

    No idea

--- projects/solrj-demo/src/main/java/com/developintelligence/Main.java
+++ projects/solrj-demo/src/main/java/com/developintelligence/Main.java
@@ -30,62 +30,50 @@ public class Main {
     public static void main(String[] args) throws IOException, SolrServerException {
 
         //Stand alone server example
-        String urlString = "http://localhost:8983/solr/movies";
-        SolrClient solr = new HttpSolrClient
-                .Builder(urlString).build();
+//        String urlString = "http://localhost:8983/solr/movies";
+//        SolrClient solr = new HttpSolrClient
+//                .Builder(urlString).build();
 
-        //SolrCloud Connection
-//        String zkHostString = "localhost:9983";
-//        SolrClient solr = new CloudSolrClient.Builder()
-//                .withZkHost(zkHostString).build();
+        String zkHostString = "http://localhost:9983";
+        SolrClient solr = new CloudSolrClient.Builder().withZkHost(zkHostString).build();
 
-        /************************************************
-         * Searching
-         ************************************************/
+//        SolrClient solr2 = new HttpSolrClient(urlString);
 
-        SolrDocument doc = null;
-        //Simple Search by Id
-        doc = solr.getById("tt0113497");
+        SolrDocument doc = solr.getById("tt0114709");
 
-        //SolrDocument basically wrapped Map
         String title = (String)doc.get("Title");
-        Integer metascore = (Integer)doc.getFieldValue("Metascore");
-        Collection<Object> genres = doc.getFieldValues("genres");
-        Collection<String> names = doc.getFieldNames();
+        Integer metaScore = (Integer)doc.getFieldValue("Metascore");
+        Collection genres = doc.getFieldValues("genres");
 
-        if(doc != null){
-            System.out.println("Got a doc");
-        }
-        // Simple search for toy with a few fields
         SolrQuery query = new SolrQuery();
-        query.setQuery("Title:future");
+        query.setQuery("Title:future Plot:future");
         query.setFields("id", "Title", "Plot", "Year");
         query.setRows(10);
 
-        // Sorting done with a SortClause
-        SolrQuery.SortClause sort;
-        sort = new SolrQuery.SortClause("Title",
-                SolrQuery.ORDER.asc);
-        query.setSort(sort);
+//        QueryResponse response = solr.query(query);
+//        for(SolrDocument d : response.getResults()){
+//            System.out.println(d.get("Title"));
+//        }
 
-        // Facets
+        //Facets
         query.setFacet(true);
         query.addFacetField("Year", "Rated", "genres");
         query.setFacetMinCount(1);
-        query.addNumericRangeFacet("Metascore", 1, 100, 10);
 
-        String[] sets = {"{!key=terrible}[0, 50)", "{!key=optional}[50, 70)", "{!key=worthwating}[70, 100)"};
-        query.addIntervalFacets("Metascore", sets);
-        query.setFacetLimit(100);
+        query.addNumericRangeFacet("Metascore", 0, 100, 10);
 
-        // Call solr with search
+        String[] intervals = {"{!key=horrible}[0,50)", "{!key=meh}[50, 75)", "{!key=good}[75, 90)", "{!key=great}[90, 100]"};
+        query.addIntervalFacets("Metascore", intervals);
+
         QueryResponse response = solr.query(query);
-        // Get Facet Fields
         List<FacetField> fields = response.getFacetFields();
+
+        // Year, Rated, genres
         for(FacetField ff : fields){
             String name = ff.getName();
             int valueCount = ff.getValueCount();
-            // Get individual values/counts
+            System.out.println("Name: " + name + "Count: " + valueCount);
+            // Individual values
             for(FacetField.Count fieldCount : ff.getValues()){
                 String value = fieldCount.getName();
                 long count = fieldCount.getCount();
@@ -94,89 +82,24 @@ public class Main {
 
         List<RangeFacet> ranges = response.getFacetRanges();
         for(RangeFacet range : ranges){
+
             List<RangeFacet.Count> counts = range.getCounts();
             for(RangeFacet.Count count : counts){
-                count.getValue();
-                count.getCount();
+                String value = count.getValue();
+                int valueCount = count.getCount();
             }
         }
 
-        List<IntervalFacet> intervals = response.getIntervalFacets();
-        for(IntervalFacet interval : intervals){
-            for(IntervalFacet.Count count : interval.getIntervals()){
-                count.getKey();
-                count.getCount();
+        //Intervals
+        List<IntervalFacet> intervalFacets = response.getIntervalFacets();
+        for(IntervalFacet intervalFacet : intervalFacets){
+            for(IntervalFacet.Count count : intervalFacet.getIntervals()){
+                String key = count.getKey();
+                int keyCount = count.getCount();
             }
         }
 
 
-
-        /************************************************
-         * Schema Api
-         ************************************************/
-
-        // Get all fields
-//        SchemaRequest.Fields fieldsRequest =
-//                new SchemaRequest.Fields();
-//        NamedList fieldsResponse = solr.request(fieldsRequest);
-//        List fieldsList = (List)fieldsResponse.get("fields");
-//
-//        // Getting a field
-//        SchemaRequest.Field request =
-//                new SchemaRequest.Field("Title");
-//        NamedList fieldResponse = solr.request(request);
-//
-//        // Getting a field type
-//        SchemaRequest.FieldType ft = new SchemaRequest.FieldType("text_general");
-//        NamedList ftNamedList = solr.request(ft);
-//
-//
-//        // Don't be fooled this is not extending map
-//        SimpleOrderedMap fieldMap =
-//                (SimpleOrderedMap)fieldResponse.get("field");
-//        String name = (String)fieldMap.get("name");
-//        String type = (String)fieldMap.get("type");
-//        Boolean indexed = (Boolean)fieldMap.get("indexed");
-//
-//        // Create the map for the values
-//        Map<String, Object> valueMap = new HashMap<>();
-//        valueMap.put("name", "amazon");
-//        valueMap.put("type", "string");
-//        valueMap.put("stored", true);
-//        valueMap.put("indexed", false);
-//        SchemaRequest.AddField addFieldRequest =
-//                new SchemaRequest.AddField(valueMap);
-//        //Returns the newly created field
-//        NamedList addFieldResponse = solr.request(request);
-
-//        FieldTypeDefinition fieldTypeDefinition = new FieldTypeDefinition();
-//        AnalyzerDefinition analyzerDefinition = new AnalyzerDefinition();
-//        analyzerDefinition.setFilters();
-//        fieldTypeDefinition.setAnalyzer();
-//        SchemaRequest.AddFieldType addFieldType = new SchemaRequest.AddFieldType()
-
-
-        /************************************************
-         * Indexing
-         ************************************************/
-//        ConcurrentUpdateSolrClient conSolr =
-//                new ConcurrentUpdateSolrClient.Builder(urlString).build();
-//        conSolr.setRequestWriter(new BinaryRequestWriter());
-//
-//        SolrInputDocument document = new SolrInputDocument();
-//        document.addField("Type", "movie");
-//        document.addField("id", "tt1234566");
-//        document.addField("genres",
-//                Arrays.asList("horror", "thriller"));
-//        UpdateResponse addResponse = conSolr.add(document);
-//
-//        //By default will block until hard commit is done
-//        UpdateResponse commit = solr.commit();
-//
-//        //By default will block until complete
-//        UpdateResponse optimize = solr.optimize();
-//
-        String foo = "bar";
-
+        String foo = "Bar";
     }
 }
